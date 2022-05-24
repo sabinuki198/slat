fb8829930ac317d0de3cc99430bd13047c6de0bf
sabinuki198 <dokkoi00@gmail.com>
2022-05-24 16:40:34 +0900

    ルーティング設定
    ・ルーティング設定
    ・DBマイグレート実施
    ・ER図修正

--- db/migrate/20220524065006_create_contents.rb
+++ db/migrate/20220524065006_create_contents.rb
@@ -1,7 +1,9 @@
 class CreateContents < ActiveRecord::Migration[6.0]
   def change
     create_table :contents do |t|
-
+      t.string :title,           null: false
+      t.text :text,              null: false
+      t.integer :category_id,    null: false
       t.timestamps
     end
   end
