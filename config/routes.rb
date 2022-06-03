0417ca331589a64b99e22ac11fcb6d69510e0cea
sabinuki198 <dokkoi00@gmail.com>
2022-06-03 17:38:17 +0900

    記事削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "contents#index"
-  resources :contents, only:[:index, :new, :create, :show, :edit, :update]
+  resources :contents
 end
