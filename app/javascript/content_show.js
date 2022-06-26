06071c1db429646ce5ae401a8712ac6ec053c66c
sabinuki198 <dokkoi00@gmail.com>
2022-06-26 22:13:53 +0900

    記事クリック時の画面遷移を中止

--- app/javascript/content_show.js
+++ app/javascript/content_show.js
@@ -6,7 +6,14 @@ function show (){
   for (const [i, e] of contents.entries()) {
     e.id = `content-id-${i}`;
   } // カテゴリー配下の記事それぞれにIDを付与
-  
+
+  contents.forEach(function(content){
+    content.addEventListener('click', function(e){
+      e.preventDefault(); //デフォルトでの画面遷移を中止
+      const contentIdGet = document.getElementById(`${e.target.id}`)
+      console.log(contentIdGet)
+    });
+  });
  };
 
  window.addEventListener('load', show);
